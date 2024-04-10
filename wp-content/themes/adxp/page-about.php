40df1150aad7fdf8c9b7ceb953e3cbdbeee15cfb
Rakhi <rakhichandranrr@gmail.com>
2024-04-10 10:44:52 +0530

    about and wp update

---
+++ wp-content/themes/adxp/page-about.php
@@ -0,0 +1,214 @@
+<?php
+/*Template Name: About Us*/
+get_header();
+$about_adxp =  get_field('about_adxp');
+$our_vision = get_field('our_vision');
+$our_mission = get_field('our_mission');
+$our_philosophy = get_field('our_philosophy');
+$our_values = get_field('our_values');
+$why_us = get_field('why_us');
+$facts_and_figure = get_field('facts_and_figure');
+?>
+
+<section class="abt-adxp common-padd">
+  <div class="container">
+    <div class="row pdl-1 pdr-1">
+      <div class="col-lg-4">
+        <div class="abt-lg">
+          <?php if ($about_adxp['about_adxp_image']['url']) {
+          ?>
+            <img src="<?php echo $about_adxp['about_adxp_image']['url']; ?>" alt="img">
+          <?php
+          }
+          ?>
+        </div>
+      </div>
+      <div class="col-lg-8 d-flex align-items-center">
+        <div class="ad-xt-main"> <span class="upper-heading text-light"><?php echo $about_adxp['about_adxp_title']; ?></span>
+          <h1 class="adxp-des"><?php echo $about_adxp['about_adxp_headline']; ?></h1>
+        </div>
+      </div>
+      <div class="col-lg-12 mt-5">
+        <div class="ad-xp-paragraph">
+          <div class="paragraph"><?php echo $about_adxp['about_adxp_content']; ?></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="vision">
+  <div class="container">
+    <div class="row flexW pdl-1 pdr-1">
+      <div class="col-lg-6">
+        <div class="ad-xt-main"> <span class="upper-heading text-light"><?php echo $our_vision['vision_title']; ?></span>
+          <h1 class="adxp-des"><?php echo $our_vision['vision_content']; ?> </h1>
+        </div>
+      </div>
+      <div class="col-lg-6 dc-img">
+        <div class="dc-portal">
+          <?php if ($our_vision['vision_image']['url']) {
+          ?>
+            <img src="<?php echo $our_vision['vision_image']['url']; ?>" alt="img">
+          <?php
+          }
+          ?>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="mission">
+  <div class="container">
+    <div class="row flexW pdl-1 pdr-1">
+      <div class="col-lg-6 dc-img">
+        <div class="dc-portal">
+          <?php if ($our_mission['mission_image']['url']) {
+          ?>
+            <img src="<?php echo $our_mission['mission_image']['url']; ?>" alt="img">
+          <?php
+          }
+          ?>
+        </div>
+      </div>
+      <div class="col-lg-6">
+        <div class="ad-xt-main"> <span class="upper-heading text-light"><?php echo $our_mission['mission_title']; ?></span>
+          <h1 class="adxp-des"><?php echo $our_mission['mission_content']; ?></h1>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="Philosophy common-padd">
+  <div class="container">
+    <div class="row pdl-1 pdr-1">
+      <h3 class="inner-heading text-light mb-4"><?php echo $our_philosophy['our_philosophy_title']; ?></h3>
+      <div class="col-lg-12">
+        <div class="paragraph"> <?php echo $our_philosophy['our_philosophy_short_description']; ?></div>
+      </div>
+      <?php
+      $our_philosophy_sub = $our_philosophy['our_philosophy_sub_content'];
+      if ($our_philosophy_sub) {
+        foreach ($our_philosophy_sub as $our_philosophy_sub_res) {
+      ?>
+          <div class="col-lg-6">
+            <h5 class="text-light"><?php echo $our_philosophy_sub_res['our_philosophy_sub_title']; ?></h5>
+            <div class="paragraph"> <?php echo $our_philosophy_sub_res['our_philosophy_sub_content']; ?></div>
+          </div>
+      <?php
+        }
+      }
+      ?>
+    </div>
+  </div>
+</section>
+<section class="Values common-padd">
+  <div class="container">
+    <div class="row pdl-1 pdr-1">
+      <h3 class="inner-heading text-light mb-4"><?php echo $our_values['our_values_title']; ?></h3>
+      <div class="col-lg-12 mb-5">
+        <div class="paragraph"> <?php echo $our_values['our_values_short_description']; ?> </div>
+      </div>
+      <?php
+      $our_values_sub = $our_values['our_values_sub_content'];
+      if ($our_values_sub) {
+        foreach ($our_values_sub as $our_values_sub_res) {
+      ?>
+          <div class="col-lg-4">
+            <div class="values-grid">
+              <?php if ($our_values_sub_res['our_values_sub_icon']['url']) {
+              ?>
+                <img src="<?php echo $our_values_sub_res['our_values_sub_icon']['url']; ?>" alt="img">
+              <?php
+              }
+              ?>
+              <h5 class="text-light"><?php echo $our_values_sub_res['our_values_sub_title']; ?></h5>
+              <div class="paragraph"> <?php echo $our_values_sub_res['our_values_sub_content']; ?></div>
+            </div>
+          </div>
+      <?php
+        }
+      }
+      ?>
+    </div>
+  </div>
+</section>
+<section class="common-padd why-us">
+  <div class="container">
+    <div class="row pdl-1 pdr-1">
+      <div class="col-lg-12">
+        <div class="ad-xt-main"> <span class="upper-heading text-light"><?php echo $why_us['why_us_title']; ?></span>
+          <h1 class="adxp-des"><?php echo $why_us['why_us_content']; ?> </h1>
+        </div>
+      </div>
+      <?php
+      $why_us_sub = $why_us['why_us_sub_items'];
+      if ($why_us_sub) {
+        foreach ($why_us_sub as $why_us_sub_res) {
+      ?>
+          <div class="col-lg-6">
+            <h5 class="text-light"><?php echo $why_us_sub_res['why_us_sub_title']; ?></h5>
+            <div class="paragraph"> <?php echo $why_us_sub_res['why_us_sub_content']; ?> </div>
+          </div>
+      <?php
+        }
+      }
+      ?>
+    </div>
+  </div>
+</section>
+<section class="Facts_and_Figures common-padd">
+  <div class="container">
+    <div class="row pdl-1 pdr-1">
+      <h3 class="inner-heading text-light mb-4"><?php echo $facts_and_figure['facts_and_figure_title']; ?></h3>
+      <div class="col-lg-12 mb-5">
+        <div class="paragraph"> <?php echo $facts_and_figure['facts_and_figure_short_description']; ?> </div>
+      </div>
+      <?php
+      $facts_and_figure_sub = $facts_and_figure['facts_and_figure_sub_items'];
+      if ($facts_and_figure_sub) {
+        foreach ($facts_and_figure_sub as $facts_and_figure_sub_res) {
+      ?>
+          <div class="col-lg-6">
+            <div class="facts-grid">
+              <h1><?php echo $facts_and_figure_sub_res['figure']; ?></h1>
+              <div class="paragraph"><?php echo $facts_and_figure_sub_res['figure_content']; ?></div>
+            </div>
+          </div>
+      <?php
+        }
+      }
+      ?>
+    </div>
+  </div>
+</section>
+<?php
+$leadership_team = get_field('select_leadership_team');
+if ($leadership_team) {
+?>
+  <section class="peoples common-padd">
+    <div class="container">
+      <div class="row pdl-1 pdr-1">
+        <div class="col-lg-12">
+          <h3 class="inner-heading text-light mb-4"> <?php echo get_field('leadership_team_heading'); ?></h3>
+        </div>
+        <?php foreach ($leadership_team as $leadership_team_res) { ?>
+          <div class="col-lg-4">
+            <div class="peoples-grid  text-light mb-5">
+              <div class="industries-img"> <img src="<?php echo wp_get_attachment_url(get_post_thumbnail_id($leadership_team_res->ID), 'full'); ?>" alt="img"> </div>
+              <h4 class="mb-2 mt-3"><?php echo $leadership_team_res->post_title; ?></h4>
+              <span class="designation "><?php echo get_field('designation', $leadership_team_res->ID); ?></span>
+              <div class="paragraph mb-3 mt-3"><?php echo $leadership_team_res->post_content; ?> </div>
+            </div>
+          </div>
+        <?php
+        }
+        ?>
+      </div>
+    </div>
+  </section>
+<?php
+}
+?>
+<?php
+get_footer();
+?>
--- wp-content/themes/adxp/template-parts/header/site-header.php
+++ wp-content/themes/adxp/template-parts/header/site-header.php
@@ -17,6 +17,10 @@ if (is_singular('post-insights')) {
 } else if (is_single() || is_page('insights')) {
   $section_cls = 'detail_page';
   $attr = '';
+} else if ( is_page('about')) {
+  $section_cls = 'detail_page about-banner ';
+  $attr = '';
+
 } else {
   $section_cls = '';
   $attr = 'id="banner_section" style="background: url(' . banner_image() . ');  background-position: center;background-size: cover;"';
@@ -25,43 +29,43 @@ if (is_singular('post-insights')) {
 $banner_video = get_field('banner_video', get_the_ID());
 if ($banner_video['url']) {
 ?>
-  <section class="banner-vw" id="banner_section">
-    <video class="video-banner" autoplay muted loop>
-      <source src="<?php echo $banner_video['url']; ?>" type="video/mp4">
-      Your browser does not support the video tag.
-    </video>
-  <?php
+
+<section class="banner-vw" id="banner_section">
+<video class="video-banner" autoplay muted loop>
+  <source src="<?php echo $banner_video['url']; ?>" type="video/mp4">
+  Your browser does not support the video tag. </video>
+<?php
 } else {
   ?>
-    <section class="banner-vw <?php echo $section_cls; ?>" <?php echo $attr; ?>>
-    <?php
+<section class="banner-vw <?php echo $section_cls; ?>" <?php echo $attr; ?>>
+  <?php
   }
     ?>
-    <header id="myHeader">
-      <div class="container">
-        <nav class="navbar navbar-expand-lg ">
-          <div class="container-fluid p-0 m-0">
-            <?php get_template_part('template-parts/header/site-branding'); ?>
-            <?php get_template_part('template-parts/header/site-nav'); ?>
-          </div>
-        </nav>
-      </div>
-    </header>
-    <section class="mobile-header p-2" id="dynamic">
-      <div class="container">
-        <div class="row">
-          <div class="mob-head d-flex justify-content-between align-items-center">
-            <button class="mobile-menu-icon" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample"> <i class="bi bi-list"></i> </button>
-            <a class="navbar-brand" href="<?php echo esc_url(home_url('/')); ?>"><img src="<?php echo $image[0]; ?>" alt="logo"> </a>
-            <button class="Search" type="submit"><i class="bi bi-search"></i></button>
-          </div>
+  <header id="myHeader">
+    <div class="container">
+      <nav class="navbar navbar-expand-lg ">
+        <div class="container-fluid p-0 m-0">
+          <?php get_template_part('template-parts/header/site-branding'); ?>
+          <?php get_template_part('template-parts/header/site-nav'); ?>
+        </div>
+      </nav>
+    </div>
+  </header>
+  <section class="mobile-header p-2" id="dynamic">
+    <div class="container">
+      <div class="row">
+        <div class="mob-head d-flex justify-content-between align-items-center">
+          <button class="mobile-menu-icon" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample"> <i class="bi bi-list"></i> </button>
+          <a class="navbar-brand" href="<?php echo esc_url(home_url('/')); ?>"><img src="<?php echo $image[0]; ?>" alt="logo"> </a>
+          <button class="Search" type="submit"><i class="bi bi-search"></i></button>
         </div>
       </div>
-    </section>
-
-    <!--BANNER STARTS-->
-
-    <?php
+    </div>
+  </section>
+  
+  <!--BANNER STARTS-->
+  
+  <?php
 
     if (!is_singular('post-insights')) {
 
@@ -77,50 +81,65 @@ if ($banner_video['url']) {
           $type = '';
         }
     ?>
-        <div class="container b_contents-main flexW inner-banner-content">
-          <div class="row">
-            <div class="banner-content d-flex flex-column align-items-center justify-content-center">
-              <div class="flex-column">
-                <?php if ($type) {
+  <div class="container b_contents-main flexW inner-banner-content">
+    <div class="row">
+      <div class="banner-content d-flex flex-column align-items-center justify-content-center">
+        <div class="flex-column">
+          <?php if ($type) {
                 ?>
-                  <span class="upper-heading text-light"><?php echo $type; ?></span>
-                <?php
+          <span class="upper-heading text-light"><?php echo $type; ?></span>
+          <?php
                 }
                 ?>
-                <?php
+          <?php
                 if (is_page('insights')) {
                 ?>
-                  <h1 class="main-tittle mb-5 text-light mt-4 insight-head"><img class="head-arrow" src="<?php echo get_template_directory_uri(); ?>/assets/img/arrow.svg" alt="img-icon"><?php echo $title; ?></h1>
-                  <div class="paragraph"> <?php echo banner_content(); ?> </div>
-                <?php
+          <h1 class="main-tittle mb-5 text-light mt-4 insight-head"><img class="head-arrow" src="<?php echo get_template_directory_uri(); ?>/assets/img/arrow.svg" alt="img-icon"><?php echo $title; ?></h1>
+          <div class="paragraph"> <?php echo banner_content(); ?> </div>
+          <?php
                 } else {
                 ?>
-                  <h1 class="main-tittle mb-5 text-light mt-4"><img class="head-arrow" src="<?php echo get_template_directory_uri(); ?>/assets/img/arrow.svg" alt="img-icon"><i><?php echo $result[0]; ?></i> <?php echo $result[1]; ?> </h1>
-                <?php
+          <h1 class="main-tittle mb-5 text-light mt-4"><img class="head-arrow" src="<?php echo get_template_directory_uri(); ?>/assets/img/arrow.svg" alt="img-icon"><i><?php echo $result[0]; ?></i> <?php echo $result[1]; ?> </h1>
+          <?php
                 }
                 ?>
-              </div>
-            </div>
-          </div>
         </div>
-      <?php
+      </div>
+    </div>
+  </div>
+  <?php
+	  }else if(is_page('about')){
+	  ?>
+  <div class="container b_contents-main flexW inner-banner-content pdl-1 pdr-1">
+    <div class="row">
+      <div class="banner-content d-flex flex-column align-items-center justify-content-center">
+        <div class="flex-column" > <span class="upper-heading text-light"><?php echo banner_title(); ?></span>
+          <h1 class="main-tittle mb-5 text-light mt-4" ><img class="head-arrow" src="<?php echo get_template_directory_uri(); ?>/assets/img/arrow.svg" alt="img-icon" ><?php echo get_field('banner_headline'); ?></h1>
+          <div class="paragraph"> <?php echo get_field('banner_short_description'); ?> </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <?php
+	  
+	  
       } else {
       ?>
-        <div class="container b_contents-main flexW">
-          <div class="row">
-            <div class="banner-content d-flex flex-column align-items-center justify-content-center">
-              <div class="flexW flex-column">
-                <h1 class="mb-4 text-center"><?php echo banner_title(); ?></h1>
-                <div class="paragraph"><?php echo banner_content(); ?></div>
-              </div>
-            </div>
-          </div>
+  <div class="container b_contents-main flexW">
+    <div class="row">
+      <div class="banner-content d-flex flex-column align-items-center justify-content-center">
+        <div class="flexW flex-column">
+          <h1 class="mb-4 text-center"><?php echo banner_title(); ?></h1>
+          <div class="paragraph"><?php echo banner_content(); ?></div>
         </div>
-    <?php
+      </div>
+    </div>
+  </div>
+  <?php
       }
     }
     ?>
-
-    <!-- BANNER ENDS-->
-
-    </section>
+  
+  <!-- BANNER ENDS--> 
+  
+</section>
