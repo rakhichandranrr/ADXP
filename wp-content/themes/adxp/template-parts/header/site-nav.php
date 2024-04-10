598c7b41885ded566fd3473910162051ce89a4b8
Rakhi <rakhichandranrr@gmail.com>
2024-04-10 14:09:01 +0530

    menu

--- wp-content/themes/adxp/template-parts/header/site-nav.php
+++ wp-content/themes/adxp/template-parts/header/site-nav.php
@@ -16,28 +16,19 @@
   <ul class="navbar-nav me-auto mb-2 mb-lg-0">
     <?php
     $menu_items = wp_get_menu_array('Header');
-	//print_r($menu_items);
-	
+
     foreach ($menu_items as $item) {
-$child_ids =array();
+      $child_ids = array();
       if (!empty($item['children'])) {
         foreach ($item['children'] as $child) {
           $child_ids[] = $child['page_id'];
         }
       }
-//print_r($child_ids);
       if (get_the_ID() == $item['page_id'] || in_array(get_the_ID(), $child_ids)) {
         $act_cls = 'active';
-	  }
-	  else if (is_singular('post-insights') && is_page('insights')) {
-		  $act_cls = 'active';
-      } 
-	  else {
+      } else {
         $act_cls = '';
       }
-	  
-	  
-	
     ?>
       <?php if (!empty($item['children'])) { ?>
         <li class="nav-item dropdown"> <a class="nav-link dropdown-toggle <?php echo $act_cls; ?>" href="<?php echo $item['url']; ?>" role="button" aria-expanded="false"> <?php echo $item['title']; ?> </a>
