e04bf57085e87e8a01db688b5bde3e3c64c52456
Rakhi <rakhichandranrr@gmail.com>
2024-04-11 15:37:24 +0530

    single digital adv

--- wp-content/themes/adxp/single-digital_advisor_asse.php
+++ wp-content/themes/adxp/single-digital_advisor_asse.php
@@ -3,50 +3,31 @@ get_header();
 ?>
 
 <section class="innovation common-padd bg-light " id="Overview">
-           <div class="container mt-4">
-               <div class="row pdl-1 pdr-1">
-                   <div class="col-lg-12"><h3 class="inner-heading mb-5">Our Values</h3></div>
-                   <div class="col-lg-3">
-                       <div class="values-grid keyareas">
-                          <img src="assets/img/k3.svg" alt="img">
-                          <h5 class="kyhd">Current Systems</h5>
-                          <div class="paragraph">
-                          Evaluates if technology systems are updated, intuitive, and incorporate real-time analytics for optimal government operation and decision-making.
-                          </div>
-                      </div>
-                   </div>
-                   <div class="col-lg-3">
-                       <div class="values-grid keyareas">
-                          <img src="assets/img/k4.svg" alt="img">
-                          <h5 class="kyhd">Agile Practices</h5>
-                          <div class="paragraph">
-                          Assesses the adoption of Agile methodologies, team collaboration, and structured project management to enhance efficiency and delivery.
-                          </div>
-                      </div>
-                   </div>
-                   <div class="col-lg-3">
-                       <div class="values-grid keyareas">
-                          <img src="assets/img/k1.svg" alt="img">
-                          <h5 class="kyhd">Technology Infrastructure</h5>
-                          <div class="paragraph">
-                          Reviews the robustness of technology infrastructure, focusing on automation, disaster recovery, auditing, and proactive monitoring for improvements.
-                          </div>
-                      </div>
-                   </div>
-                   <div class="col-lg-3">
-                       <div class="values-grid keyareas">
-                          <img src="assets/img/k2.svg" alt="img">
-                          <h5 class="kyhd">Strategic Alignment</h5>
-                          <div class="paragraph">
-                          Examines the alignment of digital transformation efforts with government objectives, strategic resource allocation, and the impact measurement of digital initiatives.
-                          </div>
-                      </div>
-                   </div>
-                   
-               </div>
-           </div>
-       </section>
-
+  <div class="container mt-4">
+    <div class="row pdl-1 pdr-1">
+      <div class="col-lg-12">
+        <h3 class="inner-heading mb-5"><?php echo get_field('content_heading');?></h3>
+      </div>
+      <?php
+	   $digital_adv_content = get_field('contents');
+	   if($digital_adv_content)
+	   {
+		   foreach($digital_adv_content as $digital_adv_content_res)
+		   {
+		?>
+      <div class="col-lg-3">
+        <div class="values-grid keyareas"> <img src="<?php echo $digital_adv_content_res['icon']['url'];?>" alt="img">
+          <h5 class="kyhd"><?php echo $digital_adv_content_res['title'];?></h5>
+          <div class="paragraph"> <?php echo $digital_adv_content_res['content'];?> </div>
+        </div>
+      </div>
+      <?php
+		}
+	}
+	?>
+    </div>
+  </div>
+</section>
 <?php
 get_footer();
 ?>
