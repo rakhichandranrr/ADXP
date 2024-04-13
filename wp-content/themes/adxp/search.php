6348cdde0b297c3439e0fad6db87ef1b9d21e65c
Rakhi <rakhichandranrr@gmail.com>
2024-04-13 18:33:34 +0530

    search

--- wp-content/themes/adxp/search.php
+++ wp-content/themes/adxp/search.php
@@ -13,26 +13,18 @@ get_header();
 
 
 	?>
-    
-    
- <section class="ins-inner-detail  common-padd bg-light " >    
-    
-  <div class="container b_contents-main flexW inner-banner-content">
+
+<section class="ins-inner-detail  common-padd bg-light pb-0" id="Overview">
+  <div class="container b_contents-main flexW inner-banner-content mb-0">
     <div class="row">
       <div class="banner-content d-flex flex-column align-items-center justify-content-center extra-padd">
-      <?php
-	  if ( have_posts() ) {
-		  ?>
-          <h1 class="main-tittle ins-tittle  mb-5 text-light mt-4"><?php
-			printf(
-				/* translators: %s: Search term. */
-				esc_html__( 'Results for "%s"', 'adxp' ),
-				'<span class="page-description search-term">' . esc_html( get_search_query() ) . '</span>'
-			);
-			?></h1>
-          <span class="upper-heading"><?php echo date('F d, Y', strtotime($insight->post_date)); ?></span>
-          <div class="paragraph"> 
-          <?php
+        <div class="flex-column" >
+          <h1 class="main-tittle ins-tittle  mb-3 text-light mt-4" >Results for
+            <div> "<?php echo  get_search_query();?>"</div>
+          </h1>
+          <div class="loc-date f-column">
+            <div class="paragraph">
+              <?php
 		printf(
 			esc_html(
 				/* translators: %d: The number of search results. */
@@ -46,41 +38,64 @@ get_header();
 			(int) $wp_query->found_posts
 		);
 		?>
-          
-          
-          
+            </div>
           </div>
-          
-           <div class="paragraph">
-           <?php
-	// Start the Loop.
-	while ( have_posts() ) {
-		the_post();
-
-		/*
-		 * Include the Post-Format-specific template for the content.
-		 * If you want to override this in a child theme, then include a file
-		 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-		 */
-		get_template_part( 'template-parts/content/content-excerpt', get_post_format() );
-	} // End the loop.
-
-	// Previous/next page navigation.
-	adxp_the_posts_navigation();
-	?>
-           </div>
-          <?php
-} else {
-	get_template_part( 'template-parts/content/content-none' );
-}
-	  ?>
         </div>
       </div>
     </div>
   </div>
 </section>
-    
+<section class="bg-light common-padd search-result-list  pdl-1 pdr-1">
+  <div class="container">
+    <div class="row">
+      <?php
+	  if ( have_posts() ) {
+		  while ( have_posts() ) {
+			  $post_id = get_the_ID();
+			  $post = get_post($post_id);
+              $thumb_id = get_post_thumbnail_id($post_id);
+		  ?>
+      <div class="col-lg-12">
+        <div class="insights-grD search-itm  d-flex flex-column">
+          <div class="img-ins mb-3">
+            <?php
+  if($thumb_id ==  242)
+  {
+ ?>
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/assets/img/no_img_insight.png" alt="img">
+            <?php
+  }
+ else
+ {
+	 ?>
+            <img src="<?php echo wp_get_attachment_url(get_post_thumbnail_id($post_id), 'full'); ?>" alt="img"  />
+            <?php
+ }
+ ?>
+          </div>
+          <div class="s-list-details" > <span><?php echo $post->post_title;?></span>
+            <h2 class="mt-2 mb-2"><?php echo $post->post_title;?></h2>
+            <div class="paragraph mb-3"><?php the_excerpt();?> </div>
+            <span><?php echo date('F d, Y', strtotime($post->post_date)); ?></span> </div>
+        </div>
+      </div>
+      <?php
+		  }
+	  }
+	  else
+	  {
+	  ?>
+      <div class="col-lg-12">
+        <div class="insights-grD search-itm  d-flex flex-column"> No search Result Found!! </div>
+      </div>
+      <?php
+	  }
+	  ?>
+    </div>
+  </div>
+</section>
 <?php
 
 
 get_footer();
+
