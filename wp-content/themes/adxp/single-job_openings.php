fe7b283a1ac43ac20c8d9a1564d3e94222f1144f
Rakhi <rakhichandranrr@gmail.com>
2024-04-13 17:47:56 +0530

    job

--- wp-content/themes/adxp/single-job_openings.php
+++ wp-content/themes/adxp/single-job_openings.php
@@ -52,48 +52,13 @@ $job_res = get_post($post_id);
             
             <div class="row custom-grid-mobile">
                 <div class="col-lg-12">
-                    <h3 class="inner-heading mb-4" >Apply for this role</h3>
+                    <h3 class="inner-heading mb-4" > <?php echo get_field('apply_for_heading',$job_res->ID);?></h3>
                     <div class="paragraph">
-                        Lorem ipsum dolor sit amet consectetur. Et velit integer scelerisque nisl accumsan. Duis nunc cras orci integer molestie 
+                         <?php echo get_field('apply_for_description',$job_res->ID);?>
                     </div>
                 </div>
             </div>
-             <div class="row">
-                 <div class="col-lg-6">
-                     <div class="form-group">
-                         <input type="text" class="form-controlnew" placeholder="Full name">
-                     </div>
-                 </div>
-                 <div class="col-lg-6">
-                     <div class="form-group">
-                         <input type="text" class="form-controlnew" placeholder="Email">
-                     </div>
-                 </div>
-                 <div class="col-lg-12">
-                     <div class="form-group">
-                         <div class="position-relative">
-                             <select class="custom-select country-code position-absolute">
-                                <option value="+1">+1 (US)</option>
-                                <option value="+44">+44 (UK)</option>
-                                <option value="+91">+91 (India)</option>
-                                <!-- Add more options as needed -->
-                            </select>
-                        <input type="text" class="form-controlnew" id="countryInput" placeholder="Mobile Number">
-                         </div>
-                    </div>
-                 </div>
-                 <div class="col-lg-12">
-                     <label>Upload CV</label>
-                     <div class="form-group" id="drop-area">
-                        <input type="file" id="fileElem" multiple class="file" accept="image/*" onchange="handleFiles(this.files)">
-                        <label for="fileElem" class="drop-label">Drag & Drop here to upload…</label>
-                         <p><i>PDF 5MB max</i></p>
-                    </div>
-                 </div>
-                  <div class="col-lg-12">
-                      <button class="btn adxp-btn mw-200" >Apply</button>
-                 </div>
-             </div>
+             <?php echo do_shortcode('[contact-form-7 id="c1955e9" title="Careers"]');?>
          </div>
       </section>
 <?php
