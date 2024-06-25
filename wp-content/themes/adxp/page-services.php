b8fa51cc2faf5b49f223993542878fa4c827d456
Rahul <rahulsudarsan.sics@gmail.com>
2024-06-25 16:43:49 +0530

    insight popup and service livable section

--- wp-content/themes/adxp/page-services.php
+++ wp-content/themes/adxp/page-services.php
@@ -1,48 +1,25 @@
 <?php
-
 /*Template Name: Services*/
-
 get_header();
-
-
-
 ?>
 <?php
-
 $services_args = array(
-
   'numberposts' => -1,
-
   'post_type'   => 'post-services',
-
   'suppress_filters' => false,
-
 );
-
 $services = get_posts($services_args);
-
-
-
 if ($services) {
-
 ?>
-
 <section class="indistries common-padd mt-3 mob-pt" id="industries_section">
   <div class="container">
     <h1 class="main-tittle mb-5 text-light page-title"><img class="head-arrow" src="<?php echo get_template_directory_uri(); ?>/assets/img/arrow.svg" alt="img-icon"><?php echo get_field('page_heading'); ?></h1>
     <div class="paragraph text-light sub-desc mb-5 page-content"><?php echo get_field('page_description'); ?></div>
     <div class="row"> 
-      
       <!--<div class="paragraph text-light sub-desc mb-5"><?php //echo get_field('short_description'); ?></div>-->
-      
       <?php
-
         foreach ($services as $services_res) : setup_postdata($services);
-
-
-
           $imgurl = wp_get_attachment_url(get_post_thumbnail_id($services_res->ID), 'full');
-
         ?>
       <div class="col-lg-6">
         <div class="industries-grid m-wz">
@@ -54,22 +31,43 @@ if ($services) {
         </div>
       </div>
       <?php
-
         endforeach;
-
         wp_reset_postdata();
-
         ?>
     </div>
   </div>
 </section>
+<section class="digital-portal common-padd dig-home" id="digita_portal_section">
+  <div class="container">
+    <div class="row flexW">
+      <div class="col-lg-7 col-md-7 imgsz livable-wrP">
+        <h1 class="main-tittle mb-5 text-light digital-head live"><img class="head-arrow" src="https://adxp.stocker.team/wp-content/themes/adxp/assets/img/arrow.svg" alt="img-icon"><p>Liveability (adj./n.) <em><br>\li-və-bi-lǝ-tē\</em></p>
+</h1>
+<div class="height32" ></div>
+       <h6 class="sub-hd-nw">Variant of liveable</h6>
+       <h6 class="sub-hd-nw fn">adj. (liv’a•bil)</h6>
+       <h6 class="sub-hd-nw">Suitable, fit, or pleasant to live in, habitable.</h6>
+       <h6 class="sub-hd-nw fn">(n.) liv’a•bil’i•ty</h6>
+      <h4 class="livable-desc text-light">
+      The <em>quality of life,</em> where <em>smart </em>choices, <em>safe</em> accessibility to <em>personalized</em> needs, 
+      <em>data-driven</em> government <em>planning,</em> and <em>digitally</em> enabled <em>services</em> (urban living, economic 
+      prosperity, social stability, educational opportunities, cultural and recreation experiences)
+       contributes to <em>competitiveness,prosperity</em>  and overall <em>well-being</em>
+      </h3>
+      
+          
+      </div>
+      <div class="col-lg-5 col-md-5 dc-img">
+        <div class="dc-portal live">
+            <img class="mb" src="../wp-content/themes/adxp/assets/images/LiveabilityM.png" alt="img">
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
 <?php
-
 }
-
 ?>
 <?php
-
 get_footer();
-
 ?>
