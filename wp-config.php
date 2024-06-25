435bf731526bc8cbe59bbc4ede076c6be5b16de1
Rakhi <rakhichandranrr@gmail.com>
2024-06-25 09:45:12 +0530

    latest

--- wp-config.php
+++ wp-config.php
@@ -79,7 +79,7 @@ $table_prefix = 'adxp_';
  *
  * @link https://wordpress.org/documentation/article/debugging-in-wordpress/
  */
-ini_set('display_errors','Off');
+ini_set('display_errors','off');
 ini_set('error_reporting', E_ALL );
 define('WP_DEBUG', false);
 define('WP_DEBUG_DISPLAY', false);
---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:5:{s:9:"wafStatus";s:7:"enabled";s:30:"learningModeGracePeriodEnabled";i:0;s:7:"authKey";s:64:"ufe6r.,d.{(o7=Jy:8cF(lj^b*YJgeUt-]/uI8lFojN;Ng-[>Zhqj5vBO)~ zy.%";s:7:"version";s:5:"1.1.0";s:13:"attackDataKey";i:3415;}
